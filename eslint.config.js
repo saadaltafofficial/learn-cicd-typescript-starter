7aa1ad2e1f18bb4a3397bfbe9d3f31b9784ce86e
root <root@DESKTOP-LTTEQC7.>
2025-07-16 20:35:19 +0500

    add eslint-plugin-security and update linting checks

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import pluginSecurity from "eslint-plugin-security";
 
 export default defineConfig([
   {
@@ -17,4 +18,5 @@ export default defineConfig([
     languageOptions: { globals: globals.browser },
   },
   tseslint.configs.recommended,
+  pluginSecurity.configs.recommended,
 ]);
